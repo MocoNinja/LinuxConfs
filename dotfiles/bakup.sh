effc63d90682bce91f59dcec9f97d4cf2a90b543
MocoNinja <javiergonzaleztello@gmail.com>
2018-03-05 20:36:41 +0100

    Initiating to implement some pseudo-tiling capabilities
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ dotfiles/bakup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Copiando configuración de zsh..."
+cp ~/.zshrc	.zshrc 
+echo "Copiando configuración de vim..."
+cp ~/.vimrc .vimrc
+echo "Copiando configuración de OpenBox..."
+cp ~/.config/openbox/rc.xml rc.xml
+echo "Copia de archivos de configuración finalizada!"
+echo "NO OLVIDES SUBIR LOS CAMBIOS"
+echo "!!!"
