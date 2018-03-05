17d800b963167d06b59d094b9ac6a8eb67987da2
MocoNinja <javiergonzaleztello@gmail.com>
2018-03-05 20:18:35 +0100

    Añadiendo configuración personalizada de OpenBox
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,4 +4,6 @@ echo "Copiando configuración de zsh..."
 cp .zshrc ~/
 echo "Copiando configuración de vim..."
 cp .vimrc ~/
+echo "Copiando configuración de OpenBox..."
+cp rc.xml ~/.config/openbox
 echo "Copia de archivos de configuración finalizada!"
