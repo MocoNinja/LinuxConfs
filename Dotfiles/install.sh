7ff6a2298cb94ed9c32491b05589056d392d415d
MocoNinja <javiergonzaleztello@gmail.com>
2018-04-29 02:39:48 +0200

    Kubuntu 18.04
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- Dotfiles/install.sh
+++ Dotfiles/install.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
+
 echo "# --------------------------------------------------- #"
 echo "# ------ Script de respaldo de configuraciones ------ #"
 echo "# --------------------------------------------------- #"
-echo "# --------------------------------------------------- #"
-echo "copiando configuraciones..."
-echo "# --------------------------------------------------- #"
+
 echo "Copiando configuración de zsh..."
 cp .zshrc ~/
 echo "Copiando configuración de vim..."
 cp .vimrc ~/
-echo "Copiando configuración de OpenBox..."
-cp rc.xml ~/.config/openbox/rc.xml
+# echo "Copiando configuración de OpenBox..."
+# cp rc.xml ~/.config/openbox/rc.xml
 echo "Copia de archivos de configuración finalizada!"
-echo "# --------------------------------------------------- #"
-echo "copiando configuración de fuentes..."
-echo "# --------------------------------------------------- #"
-sudo cp local.conf /etc/fonts/local.conf
-cp .Xresources ~/
-xrdb -merge ~/.Xresources
-echo "fuentes configuradas..."
+# echo "# --------------------------------------------------- #"
+# echo "copiando configuración de fuentes..."
+# echo "# --------------------------------------------------- #"
+# sudo cp local.conf /etc/fonts/local.conf
+# cp .Xresources ~/
+# xrdb -merge ~/.Xresources
+# echo "fuentes configuradas..."
 echo "# --------------------------------------------------- #"
