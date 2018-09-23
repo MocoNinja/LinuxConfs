5c8e6977c955baf724c31be769272fc9a8f0dd83
MocoNinja <javiergonzaleztello@gmail.com>
2018-09-23 20:04:49 +0200

    stfu
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- Dotfiles/.bin/scripts/wsl_bootstrap.sh
+++ Dotfiles/.bin/scripts/wsl_bootstrap.sh
@@ -6,7 +6,7 @@ services="mysql apache2"
 
 for service in $services; do
 if hash $service 2>/dev/null; then
-        sudo service $service start
+        sudo service $service start >/dev/null 2>&1
     else
         echo "$service was not detected as installed. Skipping..."
     fi
